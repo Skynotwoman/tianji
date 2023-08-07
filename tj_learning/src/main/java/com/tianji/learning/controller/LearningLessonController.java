d6047b28dbd5b435efff662668ae22ef95bd7829
tjxt <tjxt@itcast.cn>
2023-08-07 19:27:30 +0900

    实现我的课表相关代码

---
+++ tj_learning/src/main/java/com/tianji/learning/controller/LearningLessonController.java
@@ -0,0 +1,20 @@
+package com.tianji.learning.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 学生课程表 前端控制器
+ * </p>
+ *
+ * @author author
+ * @since 2023-08-06
+ */
+@RestController
+@RequestMapping("/learning-lesson")
+public class LearningLessonController {
+
+}
---
+++ tj_learning/src/main/java/com/tianji/learning/service/ILearningLessonService.java
@@ -0,0 +1,16 @@
+package com.tianji.learning.service;
+
+import com.tianji.learning.domain.po.LearningLesson;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 学生课程表 服务类
+ * </p>
+ *
+ * @author author
+ * @since 2023-08-06
+ */
+public interface ILearningLessonService extends IService<LearningLesson> {
+
+}
