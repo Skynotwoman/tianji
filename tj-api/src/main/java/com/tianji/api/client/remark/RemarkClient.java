fe7abb4a7e73860fea25f787738a46fa5ed4c89a
tjxt <tjxt@itcast.cn>
2023-08-16 13:19:43 +0900

    点赞

--- tj-api/src/main/java/com/tianji/api/client/remark/RemarkClient.java
+++ tj-api/src/main/java/com/tianji/api/client/remark/RemarkClient.java
@@ -1,5 +1,6 @@
 package com.tianji.api.client.remark;
 
+import com.tianji.api.client.remark.fallback.RemarkClientFallback;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Set;
 
-@FeignClient(value = "remark-service", fallbackFactory = RemarkClient.class)
+@FeignClient(value = "remark-service", fallbackFactory = RemarkClientFallback.class)
 public interface RemarkClient {
     @GetMapping("/likes/list")
     Set<Long> isBizLiked(@RequestParam("bizIds") Iterable<Long> bizIds);
