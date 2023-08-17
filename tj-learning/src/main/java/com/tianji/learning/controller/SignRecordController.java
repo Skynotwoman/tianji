457e9edb359805a8d01e0ed5f2cfa264e32a551a
tjxt <tjxt@itcast.cn>
2023-08-17 21:29:11 +0900

    签到与积分系统

--- tj-learning/src/main/java/com/tianji/learning/controller/SignRecordController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/SignRecordController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignRecordController {
     private final ISignRecordService recordService;
 
-    @PostMapping
+    @GetMapping
     @ApiOperation("签到功能窗口")
     public SignResultVO addSignRecords(){
         return recordService.addSignRecords();
